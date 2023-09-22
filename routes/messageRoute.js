93c2839477ddf28dc404db766f50e1f143b40435
jamshaidazeemofficial <jamshaid@idevnerds.com>
2023-09-22 21:54:07 +0500

    messages get and create api handlers

--- routes/messageRoute.js
+++ routes/messageRoute.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 const mongoose = require("mongoose");
-const messageSchema = require("../models/Message");
 
+const messageSchema = require("../models/Message");
 const Message = mongoose.model("Message", messageSchema);
 
 const helper = require("../helper");
@@ -11,8 +11,32 @@ const { authenticationMiddleware } = require("../middlewares/authenticator");
 
 router.use(authenticationMiddleware);
 
+router.get("/", async (req, res, next) => {
+  const senderId = req.query.senderId;
+  const recieverId = req.query.recieverId;
+
+  try {
+    // find all messages against matching sender id and reciever id
+
+    const orConditions = [
+      { senderId: senderId, recieverId: recieverId },
+      { senderId: recieverId, recieverId: senderId },
+    ];
+
+    const messages = await Message.find({
+      $or: orConditions,
+    });
+
+    res.status(200).json({
+      message: "messages list fetched successfully",
+      data: messages && messages.length ? messages : [],
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
 router.post("/create", async (req, res, next) => {
-  console.log("🚀 ~ file: messageRoute.js:15 ~ router.post ~ req:", req.body);
   const senderId = req.body.senderId;
   const recieverId = req.body.recieverId;
   const content = req.body.message;
